44f1cd144372ea6bed9100dd6273df355ca2bc10
Nelson Christovam Neto <netoncn@hotmail.com>
2021-06-23 18:01:41 -0300

    Aula 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { Button } from "./components/Button"
+import { BrowserRouter, Route } from "react-router-dom"
+import { AuthContextProvider } from './contexts/AuthContext'
+
+import { Home } from "./pages/Home"
+import { NewRoom } from "./pages/NewRoom"
 
 const App = () => {
   return (
     <>
-      <Button />
-      <Button />
+      <BrowserRouter>
+        <AuthContextProvider>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+        </AuthContextProvider>
+      </BrowserRouter>
     </>
   )
 }
